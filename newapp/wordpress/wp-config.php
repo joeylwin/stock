be46d80fd9d06f627d710e9a7e39fca14cc845f0
zhouyi126 <zhouyi7@jd.com>
2016-12-27 19:41:49 +0800

    test

--- newapp/wordpress/wp-config.php
+++ newapp/wordpress/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'app_cccjoey');
 
 /** MySQL数据库用户名 */
-define('DB_USER', 'username_here');
+define('DB_USER', 'jjkx4xoo0y');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', '15i4yzjh14mlkyhy1i4mj22jill3jxiixjll5zk5');
 
 /** MySQL主机 */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '	w.rdc.sae.sina.com.cn');
 
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8');
